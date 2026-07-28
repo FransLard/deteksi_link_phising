29a40601a4388c681f006c6c701cc11b27e60a39
frans <braxyjr4@gmail.com>
2026-07-28 17:13:56 +0700

    deteksi phishing

---
+++ phising_detector/__main__.py
@@ -0,0 +1,8 @@
+import sys
+
+if len (sys .argv )>1 and sys .argv [1 ]=="gui":
+    from .gui import main as gui_main
+    gui_main ()
+else :
+    from .cli import main as cli_main
+    cli_main ()
